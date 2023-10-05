4359c88afeda5a39e898b0ff3140be74f736ef27
47thommy <47thommy@gmail.com>
2023-10-05 10:32:06 +0300

    added some styling

--- frontend/index.js
+++ frontend/index.js
@@ -6,9 +6,61 @@ const table = document.getElementById("table");
 const noItem = document.getElementById("no-item");
 const tableBody = document.getElementById("body");
 
+// Function to fetch and display expenses
+const getExpenses = async () => {
+  const expenses = await fetch("http://localhost:5000/api/v1/expense", {
+    method: "GET",
+  });
+
+  const json = await expenses.json();
+  if (expenses.ok) {
+    tableBody.innerHTML = "";
+  }
+  if (json.length > 0) {
+    noItem.style.display = "none"; // Hide the "No Expenses Added Yet!" message
+  }
+  json.map((data) => {
+    const newRow = document.createElement("tr");
+    const newName = document.createElement("td");
+    const newDate = document.createElement("td");
+    const newAmount = document.createElement("td");
+    const deleteRow = document.createElement("td");
+    const deleteBtn = document.createElement("button");
+    deleteBtn.setAttribute("class", "remove");
+    deleteBtn.setAttribute("id", data._id);
+
+    const deleteValue = document.createTextNode("X");
+    deleteBtn.appendChild(deleteValue);
+    const dateObj = new Date(data.date);
+    const options = { year: "numeric", month: "long", day: "numeric" };
+    const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
+      dateObj
+    );
+    const nameValue = document.createTextNode(data.name);
+    const dateValue = document.createTextNode(formattedDate);
+    const amountValue = document.createTextNode(data.amount + " Birr");
+    deleteRow.appendChild(deleteBtn);
+    newName.appendChild(nameValue);
+    newDate.appendChild(dateValue);
+    newAmount.appendChild(amountValue);
+    newRow.append(newName);
+    newRow.append(newDate);
+    newRow.append(newAmount);
+    newRow.append(deleteRow);
+
+    tableBody.appendChild(newRow);
+  });
+};
+
+// Call getExpenses when the page loads
+window.addEventListener("load", () => {
+  getExpenses();
+});
+
+// Add event listener for "Add Expense" button
 button.addEventListener("click", async (e) => {
   if (Name.value == "" || date.value == "" || amount.value == "") {
-    alert("you have to provido the name, amount and date");
+    alert("You have to provide the name, amount, and date");
     return;
   }
 
@@ -23,61 +75,21 @@ button.addEventListener("click", async (e) => {
       "Content-Type": "application/json",
     },
   });
-  const json = response.json();
 
-  Name.value = "";
-  date.value = "";
-  amount.value = "";
-  getExpenses();
+  if (response.ok) {
+    Name.value = "";
+    date.value = "";
+    amount.value = "";
+    getExpenses();
+  } else {
+    console.error("Failed to add expense");
+  }
 });
-const getExpenses = async () => {
-  const expenses = await fetch("http://localhost:5000/api/v1/expense", {
-    method: "GET",
-  });
 
-  const json = await expenses.json();
-  if (expenses.ok) {
-    tableBody.innerHTML = "";
-  }
-  if (json.length > 0) {
-    noItem.setAttribute("class", "item");
-  }
-  json.map((data) => {
-    const newRow = document.createElement("tr");
-    const newName = document.createElement("td");
-    const newDate = document.createElement("td");
-    const newAmount = document.createElement("td");
-    const deleteRow = document.createElement("td");
-    const deleteBtn = document.createElement("button");
-    deleteBtn.setAttribute("class", "remove");
-    deleteBtn.setAttribute("id", data._id);
-
-    const deleteValue = document.createTextNode("X");
-    deleteBtn.appendChild(deleteValue);
-    const date = new Date(data.date);
-    const options = { year: "numeric", month: "long", day: "numeric" };
-    const formattedDate = new Intl.DateTimeFormat("en-US", options).format(
-      date
-    );
-    const nameValue = document.createTextNode(data.name);
-    const dateValue = document.createTextNode(formattedDate);
-    const amountValue = document.createTextNode(data.amount);
-    deleteRow.appendChild(deleteBtn);
-    newName.appendChild(nameValue);
-    newDate.appendChild(dateValue);
-    newAmount.appendChild(amountValue);
-    newRow.append(newName);
-    newRow.append(newDate);
-    newRow.append(newAmount);
-    newRow.append(deleteRow);
-
-    tableBody.appendChild(newRow);
-  });
-};
-
+// Add event listener for table (deleting expenses)
 table.addEventListener("click", async (e) => {
   if (e.target.classList.contains("remove")) {
-    if (confirm("are you sure you want to remove this expense?")) {
+    if (confirm("Are you sure you want to remove this expense?")) {
       await fetch("http://localhost:5000/api/v1/expense/" + e.target.id, {
         method: "DELETE",
       });
@@ -87,4 +99,3 @@ table.addEventListener("click", async (e) => {
     }
   }
 });
-getExpenses();
